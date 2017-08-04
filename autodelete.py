91929043ad642d94c5af5f88ee0481b78a8988b8
Vladislav Bakaev <vladislav-bakaev@mail.ru>
2017-08-04 23:08:31 +0400

    Реализовано автоматическое удаление устаревших конкурсов

---
+++ autodelete.py
@@ -0,0 +1,25 @@
+import argparse
+import datetime
+from importlib.machinery import SourceFileLoader
+from mongoengine import connect, Q
+
+from luckybot.model.contest import Contest
+
+
+# Функция парсинга аргументов командной строки
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--delay', type=int, default=1,
+                        help='The number of days through which the last contests are deleted')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    config = SourceFileLoader('*', 'server.conf').load_module()
+    database = connect(db=config.mongo_database, host=config.mongo_host, port=int(config.mongo_port),
+                       username=config.mongo_username, password=config.mongo_password)
+    current_date = datetime.datetime.now()
+    current_date -= datetime.timedelta(days=args.delay, hours=current_date.hour, minutes=current_date.minute,
+                                       seconds=current_date.second, microseconds=current_date.microsecond)
+    Contest.objects(Q(date__lt=current_date)).delete()
